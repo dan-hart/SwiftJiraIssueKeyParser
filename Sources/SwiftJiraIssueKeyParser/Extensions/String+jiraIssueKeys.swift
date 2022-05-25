49ce040f9714048838ee1e1fc21d9c21bd56b450
Dan Hart <dan-hart@users.noreply.github.com>
2022-05-25 13:43:38 -0400

    Remove public

--- Sources/SwiftJiraIssueKeyParser/Extensions/String+jiraIssueKeys.swift
+++ Sources/SwiftJiraIssueKeyParser/Extensions/String+jiraIssueKeys.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public extension String {
     /// parse issue key ids from this string if there are any
-    public var jiraIssueKeys: [JiraIssueKey]? {
+    var jiraIssueKeys: [JiraIssueKey]? {
         return Array<JiraIssueKey>(string: self)
     }
 }
