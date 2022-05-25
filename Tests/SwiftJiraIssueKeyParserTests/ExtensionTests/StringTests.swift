a4b633620f8feeeecbdbe3dfbc242943a01ea4c4
Dan Hart <dan-hart@users.noreply.github.com>
2022-05-25 13:44:21 -0400

    Remove //, Remove //  File.swift, Remove //  Created by Dan Hart on 5/25/22., Remove import Foundation, Add //, Add //  StringTests.swift, Add //  Created by Dan Hart on 5/25/22., Add import Foundation, Add import XCTest, Add @testable import SwiftJiraIssueKeyParser, Add class StringTests: XCTestCase {, Add override func setUp() async throws {, Add SwiftJiraIssueKeyParser.shared.instanceBaseURL = nil, Add }

---
+++ Tests/SwiftJiraIssueKeyParserTests/ExtensionTests/StringTests.swift
@@ -0,0 +1,16 @@
+//
+//  StringTests.swift
+//  
+//
+//  Created by Dan Hart on 5/25/22.
+//
+
+import Foundation
+
+import XCTest
+@testable import SwiftJiraIssueKeyParser
+
+class StringTests: XCTestCase {
+    override func setUp() async throws {
+        SwiftJiraIssueKeyParser.shared.instanceBaseURL = nil
+    }
