82f744c2f8b6285ad3bda3c4400fdbac65529edf
Dan Hart <dan-hart@users.noreply.github.com>
2022-05-25 13:31:40 -0400

    Switch url to getURL

--- Sources/SwiftJiraIssueKeyParser/Models/JiraIssueKey.swift
+++ Sources/SwiftJiraIssueKeyParser/Models/JiraIssueKey.swift
@@ -74,7 +74,7 @@ extension JiraIssueKey {
     /// Create a URL using the given jira instance url
     /// - Parameter instanceBaseURL: i.e. "https://instance.jira.com/"
     /// - Returns: a URL with the proper components added to view this issue
-    public func url() throws -> URL? {
+    public func getURL() throws -> URL? {
         if let baseURL = SwiftJiraIssueKeyParser.shared.instanceBaseURL {
             return url(using: baseURL)
         } else {
