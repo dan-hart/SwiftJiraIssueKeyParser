65382212d41860a634f1dba2253064c4bd2c638e
Dan Hart <dan-hart@users.noreply.github.com>
2022-05-25 13:45:45 -0400

    Add import Foundation, Add @testable import SwiftJiraIssueKeyParser, Switch setUpWithError to setUp, Remove throws {, Remove // Put setup code here. This method is called before the invocation of each test method in the class., Remove }, Switch override func tearDownWithError() to async, Switch // Put teardown code here to SwiftJiraIssueKeyParser, Add shared.instanceBaseURL, Switch This method is called after the invocation of each test method in the to =, Switch class. to nil, Remove func testExample() throws {, Remove // This is an example of a functional test case., Remove // Use XCTAssert and related functions to verify your tests produce the correct results., Remove // Any test you write for XCTest can be annotated as throws and async., Remove // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error., Remove // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards., Switch testPerformanceExample to testJiraIssueKeys, Remove throws, Switch // This to XCTAssertEqual("TEST-1,, Switch is to TEST-2,, Switch an to and, Switch example to TEST-3".jiraIssueKeys,, Switch of to [, Remove a, Remove performance, Remove test, Remove case., Switch self.measure to JiraIssueKey(id: "TEST-1")!,, Remove {, Add JiraIssueKey(id: "TEST-2")!,, Remove //, Remove Put, Remove the, Remove code, Remove you, Remove want, Remove to, Remove measure, Remove time, Remove of, Switch here. to JiraIssueKey(id: "TEST-3")!,, Switch } to ])

--- Tests/SwiftJiraIssueKeyParserTests/ExtensionTests/ArrayTests.swift
+++ Tests/SwiftJiraIssueKeyParserTests/ExtensionTests/ArrayTests.swift
@@ -5,31 +5,21 @@
 //  Created by Dan Hart on 5/25/22.
 //
 
+import Foundation
+
 import XCTest
+@testable import SwiftJiraIssueKeyParser
 
 class ArrayTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() async throws {
+        SwiftJiraIssueKeyParser.shared.instanceBaseURL = nil
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testJiraIssueKeys() {
+        XCTAssertEqual("TEST-1, TEST-2, and TEST-3".jiraIssueKeys, [
+            JiraIssueKey(id: "TEST-1")!,
+            JiraIssueKey(id: "TEST-2")!,
+            JiraIssueKey(id: "TEST-3")!,
+        ])
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
