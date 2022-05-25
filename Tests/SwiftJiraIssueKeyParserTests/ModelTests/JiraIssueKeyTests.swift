8c093b14ddf39806238d1b3d25a9c49711ad41a7
Dan Hart <dan-hart@users.noreply.github.com>
2022-05-25 13:47:03 -0400

    Add XCTAssertNotEqual(JiraIssueKey(id: "SMART-1"), JiraIssueKey(id: "SMART-2"))

--- Tests/SwiftJiraIssueKeyParserTests/ModelTests/JiraIssueKeyTests.swift
+++ Tests/SwiftJiraIssueKeyParserTests/ModelTests/JiraIssueKeyTests.swift
@@ -161,5 +161,6 @@ class JiraIssueKeyTests: XCTestCase {
     // MARK: - Equatable
     func testEquality() {
         XCTAssertEqual(JiraIssueKey(id: "SMART-2"), JiraIssueKey(id: "SMART-2"))
+        XCTAssertNotEqual(JiraIssueKey(id: "SMART-1"), JiraIssueKey(id: "SMART-2"))
     }
 }
